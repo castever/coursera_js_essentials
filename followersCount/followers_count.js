25735b0fbc5f6e024beb1e9a36b23706a25b0b27
Chad Stever <castever@gmail.com>
2026-04-14 00:04:05 -0400

    Create Follow Button With Count simulate instagram-like followers shows functions particularily function hoisting

---
+++ followersCount/followers_count.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+/**
+ * display the count in HTML
+ * don't know why they are using innerHTML
+ * there are risks of injection; however, because we know 
+ * it will only be number controlled by us, it should be ok
+ */
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue() {
+    if(count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count == 20) {
+        alert("Your Instagram post gained 20 followers!  Keep it up!");
+    }
+}
