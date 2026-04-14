5952334cde917f398a8f922a2f75536a063ab258
Chad Stever <castever@gmail.com>
2026-04-14 00:07:50 -0400

    Add reset count button
    reset count to 0 and alert the user

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -19,7 +19,15 @@ function displayCount() {
 function checkCountValue() {
     if(count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
-    } else if (count == 20) {
+    } else if (count === 20) {
         alert("Your Instagram post gained 20 followers!  Keep it up!");
+    } else if (count === 0) {
+        alert("The count has been reset!");
     }
+}
+
+function resetCount() {
+    count = 0;
+    displayCount();
+    checkCountValue();
 }
